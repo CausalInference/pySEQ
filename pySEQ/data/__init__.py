fa495da11bc6936a6551e39b59c6dbc026cbe811
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-11-22 22:02:15 +0100

    added LTFU - updated todos

--- pySEQ/data/__init__.py
+++ pySEQ/data/__init__.py
@@ -7,9 +7,9 @@ def load_data(name: str = "SEQdata") -> pl.DataFrame:
         if name == "SEQdata":
             data_path = loc.joinpath("SEQdata.csv")
         elif name == "SEQdata_multitreatment":
-            data_path = loc.joinpath("SEQdata_nultitreatment")
+            data_path = loc.joinpath("SEQdata_nultitreatment.csv")
         else:
-            data_path = loc.joinpath("SEQdata_LTFU")
+            data_path = loc.joinpath("SEQdata_LTFU.csv")
         return pl.read_csv(data_path)
     else:
         raise ValueError(f"Dataset '{name}' not available. Options: ['SEQdata']")
